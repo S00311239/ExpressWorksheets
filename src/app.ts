a125f790cc1a2978f206f3280749abc6618df5e0
Camila Sivirichi - STUDENT <s00311239@atu.ie>
2026-09-18 18:32:12 +0100

    Completed Express Worksheet 1

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express, {Application, Request, Response} from "express" ;
+
+const PORT = process.env.PORT || 3000;
+
+const app: Application = express();
+
+// Middleware to log requests
+app.use((req, _res, next) => {  
+    console.log(`${req.method} ${req.originalUrl}`);
+    next();
+});
+
+// Routes
+app.get("/ping", async (_req : Request, res: Response) => {
+    res.json({
+    message: "hello from Una"
+    });
+});
+
+app.get('/bananas', async (_req : Request, res: Response) => {
+    res.json({
+    message: "this is bananas",
+    });
+});
+
+// Listen request
+app.listen(PORT, () => {
+    console.log("Server is running on port", PORT);
+    });
+    
